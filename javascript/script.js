eb7784c4070397da86001684dfb93fd27b521825
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2024-03-20 18:16:32 -0300

    Finishing js file to show all pokemons of API

--- javascript/script.js
+++ javascript/script.js
@@ -44,11 +44,24 @@ const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
     return urlAsArray.at(urlAsArray.length - 2)
 })
 
-const pageLoaded = async () => {
+const pagination = (() => {
+    const limit = 30
+    let offset = 0
+
+    const getLimit = () => limit
+    const getOffset = () => offset
+    const incrementOffset = () => offset += limit
+
+
+    return {getLimit, getOffset, incrementOffset}
+})()
+
+const getPokemons = async () => {
     try {
-        const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=0')
+        const { getLimit, getOffset, incrementOffset } = pagination
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${getLimit()}&offset=${getOffset()}`)
         if(!resp.ok){
-            throw ERROR ('No information!')
+            throw new ERROR ('No information!')
         }
 
         const { results: pokeApiResults } = await resp.json()
@@ -58,12 +71,69 @@ const pageLoaded = async () => {
 
         const pokemons = idsPoke.map((id, i) => ({ id, name: pokeApiResults[i].name, types: types[i], imgUrl: img[i] }))
 
-        console.log(pokemons)
+        incrementOffset()
 
-
+        return pokemons
     } catch (error) {
         console.log('OOOOOps: ', error)
     }
+}
+
+const renderPokemons = pokemons => {
+    const ul = document.querySelector('[data-js="pokemons-list"]')
+    const fragment = document.createDocumentFragment()
+    
+    pokemons.forEach(({ id, name, types, imgUrl }) => {
+        const li = document.createElement('li')
+        const img = document.createElement('img')
+        const idNumber = document.createElement('h3')
+        const nameContainer = document.createElement('h2')
+        const typeContainer = document.createElement('p')
+        const [firstType] = types
+
+        img.setAttribute('src', imgUrl)
+        img.setAttribute('alt', name)
+        img.setAttribute('class', 'card-image')
+        li.setAttribute('class', `card ${firstType}`)
+        li.style.setProperty('--type-color', getTypeColor(firstType))
+
+        idNumber.textContent = `#${id}`
+        nameContainer.textContent = `${name[0].toUpperCase()}${name.slice(1)}`
+        typeContainer.textContent = types.length > 1 ? types.join(' | '): firstType
+        li.append(img, idNumber, nameContainer, typeContainer)
+
+        fragment.append(li)
+    });
+    ul.append(fragment)
+}
+
+const observeLastPokemon = pokemonObserver => {
+    const lastPokemon = document.querySelector('[data-js="pokemons-list"]').lastChild
+    pokemonObserver.observe(lastPokemon)
+}
+
+const handleNextPokemon = () => {
+    const pokemonObserver = new IntersectionObserver(async ([lastPokemon], observer) => {
+        if (!lastPokemon.isIntersecting){
+            return
+        }
+        observer.unobserve(lastPokemon.target)
+
+        if (pagination.getOffset >= 1302){
+            return
+        }
+
+        const pokemons = await getPokemons()
+        renderPokemons(pokemons)
+        observeLastPokemon(pokemonObserver)
+    }, { rootMargin: '500px' })
+    observeLastPokemon(pokemonObserver)
+}
+
+const pageLoaded = async () => {
+    const pokemons = await getPokemons()
+    renderPokemons(pokemons)
+    handleNextPokemon()
 }
 
 pageLoaded()
